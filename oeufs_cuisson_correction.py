7fb05b196077a302daaa6a6d44b77b79ff7edce8
xElon-Mask <xelonmask@protonmail.com>
2022-09-14 15:21:16 +0200

    print menu

---
+++ oeufs_cuisson_correction.py
@@ -0,0 +1,8 @@
+# 1ere chose a faire :afficher le menu de cuisson des oeufs
+print("Cuisson des oeufs")
+print("1 - Oeufs à la coque : 3 minutes")
+print("2 - Oeufs à la coque : 6 minutes")
+print("3 - Oeufs à la coque : 9 minutes")
+choix = input("Votre choix: ")
+
+
